f0e1b625d9c0727d542dda9cf53f3f6add0a9c52
mcueto <cueto@live.cl>
2019-07-16 20:57:05 -0400

    Add MySQL connection & query
    - Create connection with hardcoded credentials
    - Query 'SELECT * FROM people' for testing purposes
    - Data is retrieved through a promise

--- handler.js
+++ handler.js
@@ -1,16 +1,50 @@
 'use strict';
 
-module.exports.helloWorld = (event, context, callback) => {
-  const response = {
-    statusCode: 200,
-    headers: {
-      'Access-Control-Allow-Origin': '*', // Required for CORS support to work
-    },
-    body: JSON.stringify({
-      message: 'Go Serverless v1.0! Your function executed successfully!',
-      input: event,
-    }),
-  };
+const mysql = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'serverless',
+  password : 'serverless',
+  database : 'serverless-test'
+});
 
-  callback(null, response);
+const getPeopleDataPromise = new Promise((resolve, reject) => {
+  connection.connect();
+
+  connection.query('SELECT * FROM people', (error, results, fields) => {
+    if (error) throw error;
+    resolve(results);
+  });
+
+  connection.end();
+});
+
+module.exports.helloWorld = (event, context, callback) => {
+  var response = {};
+
+  getPeopleDataPromise.then(
+    (resolvedValue) => {
+      response = {
+        statusCode: 200,
+        headers: {
+          'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        },
+        body: JSON.stringify(resolvedValue),
+      };
+
+    }, (error) => {
+      response = {
+        statusCode: 400,
+        headers: {
+          'Access-Control-Allow-Origin': '*', // Required for CORS support to work
+        },
+        body: JSON.stringify(error),
+      };
+
+  }).finally(
+    () => {
+      callback(null, response);
+    }
+  );
+
 };
